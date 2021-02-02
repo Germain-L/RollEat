1d9cb4401e6b2aa8c052cea64ea51f0e6824a151
Germain-L <germain.leignel@gmail.com>
2021-02-02 16:50:19 +0100

    Created Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer color="white">
+    <v-col class="text-center" cols="12">
+      <v-row justify="center">
+        <p>Qui sommes nous ?</p>
+        <p>Contact</p>
+      </v-row>
+      {{ new Date().getFullYear() }} — <strong>RollEat</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+p {
+    font-size: 18px;
+    padding: 1em;
+}
+</style>
