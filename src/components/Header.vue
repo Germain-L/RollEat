822215801533a4743a0f88b5c9a7129136168eaf
Germain-L <germain.leignel@gmail.com>
2021-02-02 16:49:58 +0100

    Created Header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app color="white" elevation="0">
+    <v-toolbar-title>RollEat</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>Connexion</v-btn>
+
+    <v-divider class="mx-4" vertical></v-divider>
+
+    <v-btn text>Inscription</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
