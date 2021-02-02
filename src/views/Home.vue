4091961b94f4f460bca1e604aa878bd96a22b980
Germain-L <germain.leignel@gmail.com>
2021-02-02 16:50:28 +0100

    Created home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-col align="center">
+      <h1>Quel est le but de Roll Eat ?</h1>
+      <v-container class="description">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          turpis amet urna sagittis, aliquam pulvinar. Id risus lorem pharetra
+          sed. Id tempus aenean dolor, faucibus at donec sollicitudin. Ultrices
+          tellus risus magna diam in praesent elementum aliquam curabitur.
+        </p>
+      </v-container>
+      <v-img
+        :src="require('../assets/slogan.png')"
+        contain
+        height="350"
+      />
+      <v-container></v-container>
+      <CustomButton text="Commander"/>
+    </v-col>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import CustomButton from '../components/Custom_button.vue'
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    CustomButton
   }
-}
+};
 </script>
+
+<style scoped>
+h1 {
+  font-size: 50px;
+  color: grey;
+}
+
+.description {
+  width: 50%;
+}
+
+
+</style>
