c685973ebd370e594d8acaa255c57c10d29f40bd
thomasBarille-ab <thomasBarille-ab@github.com>
2021-02-04 18:03:29 +0100

    created login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,42 @@
 <template>
-  <v-row justify="center">
-      <v-col justify="center">
+  <v-container justify="center">
+      <v-col justify="center" align="center">
           <h2> Connexion </h2>
+          <v-text-field label="Email">
+            
+          </v-text-field>
+          <v-text-field 
+            :append-icon= "showPassword? 'mdi-eye': 'mdi-eye-off' " 
+            label="Password"
+            :type= "showPassword? 'text': 'password' "
+            @click:append="showPassword = !showPassword">
+           </v-text-field>
+            <CustomButton text="Connexion"/>
       </v-col>
-  </v-row>
+  </v-container>
 </template>
 
 <script>
+
+import CustomButton from "@/components/Custom_button.vue"
 export default {
     name: 'Login',
+    components: {
+        CustomButton
+    },
+
+    data() {
+        return {
+            showPassword:false,
+        }
+    }
+
 }
 </script>
 
 <style scoped>
 
+h2 {
+    text-align: center;
+}
 </style>
