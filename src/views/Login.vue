691e36ab181f83c671f1cb603286a5c3a332f663
thomasBarille-ab <thomasBarille-ab@github.com>
2021-02-04 16:15:19 +0100

    Started login

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center">
+      <v-col justify="center">
+          <h2> Connexion </h2>
+      </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+
+<style scoped>
+
+</style>
