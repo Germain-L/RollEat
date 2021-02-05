cc57c3ea2a98d6ddbbc4bc4b30c51b7ca43cf214
Stanislas Castaybert <perso@stan1712.com>
2021-02-05 16:08:27 +0100

    ✨feature: Affichage ou non de l'utilisateur dans la nav et le footer (bouton de deco aussi)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,70 @@
 <template>
-  <v-footer color="white">
-    <v-col class="text-center" cols="12">
-      <v-row justify="center">
-        <p>Qui sommes nous ?</p>
-        <p>Contact</p>
-      </v-row>
-      {{ new Date().getFullYear() }} — <strong>RollEat</strong>
-    </v-col>
-  </v-footer>
+	<v-footer color="white">
+		<v-col class="text-center" cols="12">
+			<v-row justify="center">
+					<v-btn small class="col-auto mx-2">Qui sommes nous ?</v-btn>
+					<v-btn small class="col-auto mx-2">Contact</v-btn>
+					<v-btn small color="warning" class="col-auto mx-2" v-show="loggedIn" @click="dialog = true">Se déconnecter 🥺</v-btn>
+			</v-row>
+			<br/>
+			{{ new Date().getFullYear() }} — <strong>RollEat</strong>
+		</v-col>
+
+		<v-dialog v-model="dialog" max-width="525">
+			<v-card>
+				<v-card-title class="headline">
+					Voulez-vous vraiment vous déconnecter ?
+				</v-card-title>
+				<v-card-text>
+					<iframe src="https://giphy.com/embed/THgdxkFUCAi9vFfba6" width="480" height="438" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn color="green darken-1" text @click="dialog = false">
+						NON !
+					</v-btn>
+					<v-btn color="red darken-1" text @click="signOut(), dialog = false">
+						ui
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</v-footer>
 </template>
 
 
 <script>
-export default {
-  name: "Footer",
-};
+	export default {
+		name: "Footer",
+		data() {
+			return {
+				loggedIn: false,
+				dialog: false
+			}
+		},
+		methods: {
+			async signOut() {
+				try {
+					await this.$firebase.auth().signOut();
+					this.$router.replace({
+						path: "Login"
+					});
+				} catch (err) {
+					console.log(err);
+				}
+
+				console.log("T'ES DECO BOLOSSSSSS");
+			}
+		},
+		created() {
+			this.loggedIn = this.$firebase.auth().currentUser ? true : false;
+		}
+	};
 </script>
 
 <style scoped>
 p {
-    font-size: 18px;
-    padding: 1em;
+		font-size: 18px;
+		padding: 1em;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,33 @@
 <template>
-  <v-app-bar app color="white" elevation="0">
-    <v-toolbar-title> 
-      <router-link to="/">RollEat</router-link></v-toolbar-title>
+	<v-app-bar app color="white" elevation="0">
+		<v-toolbar-title> 
+			<router-link to="/">RollEat</router-link></v-toolbar-title>
 
-    <v-spacer></v-spacer>
+		<v-spacer></v-spacer>
 
-    <router-link to="/login">Connexion</router-link>
+		<div v-if="loggedIn">
+			<router-link to="/profile">Coucou {{ $firebase.auth().currentUser.displayName }} !</router-link>
+		</div>
+		<div v-else>
+			<router-link to="/login">Connexion</router-link>
 
-    <v-divider class="mx-4" vertical></v-divider>
+			<v-divider class="mx-4" vertical></v-divider>
 
-    <router-link to="/signup">Inscription</router-link>
-  </v-app-bar>
+			<router-link to="/signup">Inscription</router-link>
+		</div>
+	</v-app-bar>
 </template>
 
 <script>
 export default {
-  name: "Header",
+	name: "Header",
+	data() {
+		return {
+			loggedIn: false,
+		}
+	},
+	created() {
+		this.loggedIn = this.$firebase.auth().currentUser ? true : false;
+	}
 };
 </script>
