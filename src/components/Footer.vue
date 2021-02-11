731885000cd272892f05021d9243907bcb00fdf4
Germain-L <germain.leignel@gmail.com>
2021-02-11 15:19:24 +0100

    changed gif + refactor

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,18 @@
 	<v-footer color="white">
 		<v-col class="text-center" cols="12">
 			<v-row justify="center">
-					<v-btn small class="col-auto mx-2">Qui sommes nous ?</v-btn>
-					<v-btn small class="col-auto mx-2">Contact</v-btn>
-					<v-btn small color="warning" class="col-auto mx-2" v-show="loggedIn" @click="dialog = true">Se déconnecter 🥺</v-btn>
+				<v-btn small class="col-auto mx-2">Qui sommes nous ?</v-btn>
+				<v-btn small class="col-auto mx-2">Contact</v-btn>
+				<v-btn
+					small
+					color="warning"
+					class="col-auto mx-2"
+					v-show="loggedIn"
+					@click="dialog = true"
+					>Se déconnecter 🥺</v-btn
+				>
 			</v-row>
-			<br/>
+			<br />
 			{{ new Date().getFullYear() }} — <strong>RollEat</strong>
 		</v-col>
 
@@ -16,14 +23,22 @@
 					Voulez-vous vraiment vous déconnecter ?
 				</v-card-title>
 				<v-card-text>
-					<iframe src="https://giphy.com/embed/THgdxkFUCAi9vFfba6" width="480" height="438" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+					<img
+						width="480"
+						src="https://i.imgur.com/OI30Xvb.jpg"
+						alt="Logout cat"
+					/>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-btn color="green darken-1" text @click="dialog = false">
 						NON !
 					</v-btn>
-					<v-btn color="red darken-1" text @click="signOut(), dialog = false">
+					<v-btn
+						color="red darken-1"
+						text
+						@click="signOut(), (dialog = false)"
+					>
 						ui
 					</v-btn>
 				</v-card-actions>
@@ -32,39 +47,38 @@
 	</v-footer>
 </template>
 
-
 <script>
-	export default {
-		name: "Footer",
-		data() {
-			return {
-				loggedIn: false,
-				dialog: false
+export default {
+	name: "Footer",
+	data() {
+		return {
+			loggedIn: false,
+			dialog: false
+		};
+	},
+	methods: {
+		async signOut() {
+			try {
+				await this.$firebase.auth().signOut();
+				this.$router.replace({
+					path: "Login"
+				});
+			} catch (err) {
+				console.log(err);
 			}
-		},
-		methods: {
-			async signOut() {
-				try {
-					await this.$firebase.auth().signOut();
-					this.$router.replace({
-						path: "Login"
-					});
-				} catch (err) {
-					console.log(err);
-				}
 
-				console.log("T'ES DECO BOLOSSSSSS");
-			}
-		},
-		created() {
-			this.loggedIn = this.$firebase.auth().currentUser ? true : false;
+			console.log("T'ES DECO BOLOSSSSSS");
 		}
-	};
+	},
+	created() {
+		this.loggedIn = this.$firebase.auth().currentUser ? true : false;
+	}
+};
 </script>
 
 <style scoped>
 p {
-		font-size: 18px;
-		padding: 1em;
+	font-size: 18px;
+	padding: 1em;
 }
 </style>
