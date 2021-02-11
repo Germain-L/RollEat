7689b544907e1f8d7f915277ccc7a1e4c070c2af
Germain-L <germain.leignel@gmail.com>
2021-02-11 15:19:33 +0100

    added buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,27 @@
 <template>
 	<v-app-bar app color="white" elevation="0">
-		<v-toolbar-title> 
-			<router-link to="/">RollEat</router-link></v-toolbar-title>
+		<v-toolbar-title>
+			<router-link to="/" class="buttons">RollEat</router-link></v-toolbar-title
+		>
 
 		<v-spacer></v-spacer>
 
 		<div v-if="loggedIn">
-			<router-link to="/profile">Coucou {{ $firebase.auth().currentUser.displayName }} !</router-link>
+			<router-link to="/profile"
+				>Coucou
+				{{ $firebase.auth().currentUser.displayName }} !</router-link
+			>
 		</div>
 		<div v-else>
-			<router-link to="/login">Connexion</router-link>
+			<router-link to="/login" class="buttons">
+				<v-btn>Connexion</v-btn>
+			</router-link>
 
 			<v-divider class="mx-4" vertical></v-divider>
 
-			<router-link to="/signup">Inscription</router-link>
+			<router-link to="/signup" class="buttons">
+				<v-btn>Inscription</v-btn>
+			</router-link>
 		</div>
 	</v-app-bar>
 </template>
@@ -23,11 +31,17 @@ export default {
 	name: "Header",
 	data() {
 		return {
-			loggedIn: false,
-		}
+			loggedIn: false
+		};
 	},
 	created() {
 		this.loggedIn = this.$firebase.auth().currentUser ? true : false;
 	}
 };
 </script>
+
+<style scoped>
+.buttons {
+	text-decoration: none;
+}
+</style>
