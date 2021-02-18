1b0c285505a17d361aaced783a11dc9af0c0417d
Theodsg <dsgetheo@gmail.com>
2021-02-18 15:23:11 +0100

    Added fonts in style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-app>
+	<v-app class="app">
 		<Header />
 		<v-main>
 			<v-divider></v-divider>
@@ -30,5 +30,19 @@ export default {
 <style>
 .buttons {
 	text-decoration: none;
+}
+
+.app {
+	font-family: Poppins, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+
+@font-face {
+	font-family: 'poppinsregular';
+	src: url('assets/fonts/poppins-regular-webfont.woff2') format('woff2'),
+	url('assets/fonts/poppins-regular-webfont.woff') format('woff');
+	font-weight: normal;
+	font-style: normal;
 }
 </style>
