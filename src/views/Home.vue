848ffc5ccf7602df65b7cca507ff869c0aa57203
Germain-L <germain.leignel@gmail.com>
2021-02-18 13:59:34 +0100

    linked page to button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,42 @@
 <template>
-  <div>
-    <v-col align="center">
-      <h1>Quel est le but de Roll Eat ?</h1>
-      <v-container class="description">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          turpis amet urna sagittis, aliquam pulvinar. Id risus lorem pharetra
-          sed. Id tempus aenean dolor, faucibus at donec sollicitudin. Ultrices
-          tellus risus magna diam in praesent elementum aliquam curabitur.
-        </p>
-      </v-container>
-      <v-img
-        :src="require('../assets/slogan.png')"
-        contain
-        height="350"
-      />
-      <v-container></v-container>
-      <CustomButton text="Commander"/>
-    </v-col>
-  </div>
+	<div>
+		<v-col align="center">
+			<h1>Quel est le but de Roll Eat ?</h1>
+			<v-container class="description">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Curabitur turpis amet urna sagittis, aliquam pulvinar. Id
+					risus lorem pharetra sed. Id tempus aenean dolor, faucibus
+					at donec sollicitudin. Ultrices tellus risus magna diam in
+					praesent elementum aliquam curabitur.
+				</p>
+			</v-container>
+			<v-img
+				:src="require('../assets/slogan.png')"
+				contain
+				height="350"
+			/>
+			<v-container></v-container>
+			<router-link to="/order" class="buttons">
+				<v-btn>Commander</v-btn>
+			</router-link>
+		</v-col>
+	</div>
 </template>
 
 <script>
-import CustomButton from '../components/Custom_button.vue'
 export default {
-  name: "Home",
-  components: {
-    CustomButton
-  }
+	name: "Home"
 };
 </script>
 
 <style scoped>
 h1 {
-  font-size: 50px;
-  color: grey;
+	font-size: 50px;
+	color: grey;
 }
 
 .description {
-  width: 50%;
+	width: 50%;
 }
-
-
 </style>
