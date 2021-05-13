7bb78472c8e8a69c160cafbbbf0bf1a633d37d47
Stanislas Castaybert <perso@stan1712.com>
2021-05-13 15:14:27 +0200

    🌈 style・Beautify

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,86 +6,39 @@
 			</v-col>
 
 			<v-col cols="12" lg="6">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Prénom"
-					v-model="userData.firstName"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Prénom" v-model="userData.firstName"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="6">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Nom"
-					v-model="userData.lastName"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Nom" v-model="userData.lastName"></v-text-field>
 			</v-col>
 
 			<v-col cols="12" lg="6">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Pseudo"
-					v-model="userData.username"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Pseudo" v-model="userData.username"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="4">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Email"
-					v-model="userData.email"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Email" v-model="userData.email"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="2">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="phone"
-					v-model="userData.phone"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Téléphone" v-model="userData.phone"></v-text-field>
 			</v-col>
 
 			<v-col cols="12" lg="6">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Rue"
-					v-model="userData.address.street"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Rue" v-model="userData.address.street"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="4">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Ville"
-					v-model="userData.address.city"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Ville" v-model="userData.address.city"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="2">
-				<v-text-field
-					class="px-0"
-					type="text"
-					label="Code postal"
-					v-model="userData.address.zipcode"
-				></v-text-field>
+				<v-text-field class="px-0" type="text" label="Code postal" v-model="userData.address.zipcode"></v-text-field>
 			</v-col>
 			<v-col cols="12" lg="12" align="center">
-				<v-btn color="success" @click="submit" outlined
-					>Sauvegarder</v-btn
-				>
+				<v-btn color="success" @click="submit" outlined>Sauvegarder</v-btn>
 			</v-col>
 			<v-snackbar v-model="snackbar" timeout="3000">
 				{{this.snackbarText}}
 
 				<template v-slot:action="{ attrs }">
-					<v-btn
-						color="blue"
-						text
-						v-bind="attrs"
-						@click="snackbar = false"
-					>
+					<v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
 						Fermer
 					</v-btn>
 				</template>
@@ -96,7 +49,7 @@
 
 <script>
 export default {
-	name: "About",
+	name: "Profile",
 	data() {
 		return {
 			userData: this.$models.user,
@@ -106,41 +59,28 @@ export default {
 	},
 	methods: {
 		submit() {
-			this.$db
-				.collection("users")
-				.doc(this.$firebase.auth().currentUser.uid)
-				.set(
-					{
-						...this.userData
-					},
-					{
-						merge: true
-					}
-				)
-				.then(() => {
-					console.log("Document successfully written!");
-					this.snackbarText = "Votre profil à été mis à jour";
-					this.snackbar = true;
-				})
-				.catch((err) => {
-					console.log(err);
-
-					this.snackbarText = err;
-					this.snackbar = true
-				})
+			this.$db.collection("users").doc(this.$firebase.auth().currentUser.uid).set({
+				...this.userData
+			}, {
+				merge: true
+			}).then(() => {
+				console.log("Document successfully written!");
+				this.snackbarText = "Votre profil à été mis à jour";
+				this.snackbar = true;
+			}).catch((err) => {
+				this.snackbarText = err;
+				this.snackbar = true
+			});
 		}
 	},
 	created() {
-		this.$db
-			.collection("users")
-			.doc(this.$firebase.auth().currentUser.uid)
-			.onSnapshot(doc => {
-				this.userData = {
-					...this.$models.user,
-					...doc.data(),
-					id: doc.id
-				};
-			});
+		this.$db.collection("users").doc(this.$firebase.auth().currentUser.uid).onSnapshot(doc => {
+			this.userData = {
+				...this.$models.user,
+				...doc.data(),
+				id: doc.id
+			};
+		});
 	}
 };
 </script>
